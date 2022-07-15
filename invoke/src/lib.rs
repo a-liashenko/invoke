b360070adbba93dd9271344c7a9ff3b980b3d5d8
Alex <icealexxx@gmail.com>
2022-07-16 03:55:50 +0400

    Export FnId type

--- invoke/src/lib.rs
+++ invoke/src/lib.rs
@@ -5,6 +5,8 @@ pub use invoke_derive::invoke;
 pub use invoke_meta::{InvokeMeta, InvokeMetaExt};
 pub use invoke_trait::{Invoke, InvokeExt};
 
+pub type FnId = u16;
+
 #[derive(Debug)]
 pub enum InvokeError {
     NoneArgs,
